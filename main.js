9ba19786e1d17ea50d2d2d58f0654342bc44f70a
mercic <christianmerci11@gmail.com>
2024-10-12 23:00:18 +0200

    changes

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
-function Person(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-  }
-  
-  // Adding a method to the prototype
-  Person.prototype.getFullName = function() {
-    return this.firstName + " " + this.lastName;
-  };
-  let person1 = new Person("John", "Doe", 25);
-let person2 = new Person("Jane", "Smith", 30);
 
-console.log(person1.getFullName()); // "John Doe"
-console.log(person2.getFullName()); // "Jane Smith"
-console.log(person1)
+// const inputs = document.querySelectorAll("input").value;
+const addBook = document.getElementById("addBook");
+const newBook = document.getElementById("newBook");
+const modals = document.querySelector(".modals");
+const allBooks = document.querySelector("#allbooks");
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".buttons");
+
+function Book(author,title,pages){
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  
+}
+
+
+let inputsArr = [];
+
+const addToArr = () => {
+  const author = document.getElementById("input1").value;
+  const title = document.getElementById("input2").value;
+  const pages = document.getElementById("input3").value;
+
+  // e.preventDefault();
+
+  const book1 = new Book(author,title,pages);
+  inputsArr.push(book1);
+  console.log(inputsArr);
+  console.log(book1);
+  modals.style.display = "none";
+  buttons.style.display = "block";
+
+}
+addBook.addEventListener("click",addToArr);
+newBook.addEventListener("click",function(){
+  modals.style.display = "block";
+  // this.style.display = "none";
+  // allBooks.style.display = "none";
+  // buttons.style.display = "none";
+
+})
+allBooks.addEventListener("click",function(){
+ inputsArr.forEach(el =>{
+  display.textContent = `Author: ${el.author} Title: ${el.title} Pages: ${el.pages}`;
+  console.log(`Author: ${el.author} Title: ${el.title} Pages: ${el.pages}`);
+ });
+ 
+})
+
